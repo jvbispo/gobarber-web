5c7b6d4deb5792ec66949921beb1401702e68024
joão victor <jv_bispo1@hotmail.com>
2020-06-02 20:04:53 -0300

    updated Dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable import/no-unresolved */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { FiPower, FiClock } from 'react-icons/fi';
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 import {
   Container,
   Header,
@@ -10,12 +13,21 @@ import {
   Content,
   Schedule,
   Calendar,
+  Section,
+  Appointment,
 } from './styles';
 import { useAuth } from '../../hooks/authContext';
 import logo from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const { signOut, user } = useAuth();
+
+  const handleDayChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available) {
+      setSelectedDate(day);
+    }
+  }, []);
   return (
     <Container>
       <Header>
@@ -58,8 +70,83 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã:</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://skylab.rocketseat.com.br/api/users/avatar/profile-dcb1b7ca-0688-4170-ab8a-8f0e470066a3.jpg"
+                  alt=""
+                />
+                <strong> João Victor</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                09:00
+              </span>
+              <div>
+                <img
+                  src="https://skylab.rocketseat.com.br/api/users/avatar/profile-dcb1b7ca-0688-4170-ab8a-8f0e470066a3.jpg"
+                  alt=""
+                />
+                <strong> João Victor</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                10:00
+              </span>
+              <div>
+                <img
+                  src="https://skylab.rocketseat.com.br/api/users/avatar/profile-dcb1b7ca-0688-4170-ab8a-8f0e470066a3.jpg"
+                  alt=""
+                />
+                <strong> João Victor</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde:</strong>
+          </Section>
         </Schedule>
-        <Calendar />
+        <Calendar>
+          <DayPicker
+            weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+            fromMonth={new Date()}
+            disabledDays={[{ daysOfWeek: [0, 6] }]}
+            modifiers={{
+              available: { daysOfWeek: [1, 2, 3, 4, 5] },
+            }}
+            selectedDays={selectedDate}
+            onDayClick={handleDayChange}
+            months={[
+              'Janeiro',
+              'Fevereiro',
+              'Março',
+              'Abril',
+              'Maio',
+              'Junho',
+              'Julho',
+              'Agosto',
+              'Setembro',
+              'Outubro',
+              'Novembro',
+              'Dezembro',
+            ]}
+          />
+        </Calendar>
       </Content>
     </Container>
   );
